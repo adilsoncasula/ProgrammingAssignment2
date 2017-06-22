833f87b051355f614e7a36819bdf70106d68e433
Adilson Casula <adilsoncasula@gmail.com>
2017-06-22 12:33:45 -0300

    Update1
    Added my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First of all, define your matrix
+# random can be done by
+alpha <- matrix(rnorm(9),3,3)
 
-## Write a short comment describing this function
-
+## than you'll work with "my_matrix$set(alpha)"
+## functions will take your matrx and set it's inverse 
+## and save it in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, 
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This one will retrieve the matrix inverse from cache if already calculatet. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("getting cache data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
